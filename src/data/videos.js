214adec42bd43d83ba98597e277ae68ea8a981ad
Abhinav M Hari <abhinav03m@gmail.com>
2025-05-13 20:53:15 +0530

    added new videos

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,11 +1,11 @@
 // src/data/videos.js
 
 const videos = [
-  { id: "video1", title: "Gems", url: "/videos/gems.mp4" },
-  { id: "video2", title: "Cricket", url: "/videos/cricket.mp4" },
+  { id: "video1", title: "Gems", url: "/videos/gems2.mp4" },
+  { id: "video2", title: "Cricket", url: "/videos/cricket2.mp4" },
   { id: "video3", title: "Cadbury", url: "/videos/cadbury.mp4" },
-  { id: "video4", title: "Snickers", url: "/videos/snickers.mp4" },
-  { id: "video5", title: "Fevicol", url: "/videos/fevicol.mp4" },
+  { id: "video4", title: "Snickers", url: "/videos/snickers2.mp4" },
+  { id: "video5", title: "Fevicol", url: "/videos/fevicol2.mp4" },
   // Add more video objects as needed
 ];
 
